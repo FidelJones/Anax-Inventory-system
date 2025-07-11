2e4a645251f923524055d2fbca9f207cdd6b97fd
FidelJones <leftoversme@gmail.com>
2025-07-12 00:56:20 +0300

    Working on settings

--- ANAX_django/anax_project/anax_project/settings.py
+++ ANAX_django/anax_project/anax_project/settings.py
@@ -47,6 +47,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'store',  # Ensure your app is included here
+    'store.apps.StoreConfig',  # Ensure your app is included here
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 ROOT_URLCONF = 'anax_project.urls'
