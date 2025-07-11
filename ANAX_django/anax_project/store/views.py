652d1930f3700c23fee317956a8fbd0fe5143089
FidelJones <leftoversme@gmail.com>
2025-07-12 00:52:09 +0300

    Superuser created and templated folder created

--- ANAX_django/anax_project/store/views.py
+++ ANAX_django/anax_project/store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.filter(stock__gt=0)
+    return render(request, 'store/product_list.html', {'products': products})
