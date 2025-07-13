1c1d2419d07e5aeb0c86abba34fa61934aa425f9
FidelJones <leftoversme@gmail.com>
2025-07-13 22:02:44 +0300

    creating the models for each model

--- ANAX_django/anax_project/accounts/models.py
+++ ANAX_django/anax_project/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
 
 # Create your models here.
+class User(AbstractUser):
+    USER_TYPES = (
+        ('customer', 'Customer'),
+        ('staff', 'Staff'),
+        ('admin', 'Admin'),
+    )
+    user_type = models.CharField(max_length=20, choices=USER_TYPES, default='customer')
+    phone_number = models.CharField(max_length=50)
+    is_verified = models.BooleanField(default=False)
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    loyalty_points = models.IntegerField(default=0)
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, default='Uganda')
+    is_default = models.BooleanField(default=False)
