900201dea9d9f4ec25a79a8ff03c4b404b9990f1
FidelJones <leftoversme@gmail.com>
2025-07-19 19:15:01 +0300

    apps registered and admin works i will start changing the model based on the cilents approval

--- ANAX_django/anax_project/orders/admin.py
+++ ANAX_django/anax_project/orders/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Order, OrderItem, OrderTracking, Address
+from .models import Order, OrderItem, OrderTracking
 
 admin.site.register(Order)
 admin.site.register(OrderItem) 
 admin.site.register(OrderTracking)
-admin.site.register(Address)
+
 
 # Register your models here.
