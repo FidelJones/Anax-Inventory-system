e476521f7f3cb66019d44b78936012c1afd3cfe9
FidelJones <leftoversme@gmail.com>
2025-07-19 18:01:12 +0300

    fixed bugs

--- ANAX_django/anax_project/store/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def product_list(request):
-    products = Product.objects.filter(stock__gt=0)
-    return render(request, 'store/product_list.html', {'products': products})
