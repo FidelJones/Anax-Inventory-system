227d448df1b12a0cb0f9f4fb670c05bada1f1783
FidelJones <leftoversme@gmail.com>
2025-07-19 19:09:12 +0300

    registeration is done

--- ANAX_django/anax_project/orders/admin.py
+++ ANAX_django/anax_project/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order, OrderItem, OrderTracking, Address
+
+admin.site.register(Order)
+admin.site.register(OrderItem) 
+admin.site.register(OrderTracking)
+admin.site.register(Address)
 
 # Register your models here.
