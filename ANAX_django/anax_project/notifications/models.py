bd825dd175ac9cd5970e61dc9cf6cec9c84c1ad5
FidelJones <leftoversme@gmail.com>
2025-07-19 18:09:56 +0300

    added models for notification

--- ANAX_django/anax_project/notifications/models.py
+++ ANAX_django/anax_project/notifications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    type = models.CharField(max_length=50)  # e.g. order_update, promo, system
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class EmailTemplate(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=150)
+    body = models.TextField()
+
+
