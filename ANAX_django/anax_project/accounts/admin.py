4da3b0ba3e1359196ad1a6021cf82d994f0cdfc6
FidelJones <leftoversme@gmail.com>
2025-07-19 19:02:30 +0300

    created superuser and now registering the models

--- ANAX_django/anax_project/accounts/admin.py
+++ ANAX_django/anax_project/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import StaffProfile, CustomerProfile, User, Address
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+admin.site.register(User, BaseUserAdmin)
+admin.site.register(CustomerProfile)
+admin.site.register(StaffProfile)
+admin.site.register(Address)
+
--- ANAX_django/anax_project/products/admin.py
+++ ANAX_django/anax_project/products/admin.py
@@ -1,3 +1,10 @@
+# products/admin.py
 from django.contrib import admin
+from .models import Category, Product, ProductImage, Inventory, ProductVariant
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Inventory)
+admin.site.register(ProductVariant)
+
